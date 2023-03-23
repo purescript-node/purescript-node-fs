07025c7904a9287e85e8f288f66f9c6725684607
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-03-22 22:14:18 -0500

    Move FileFlags to constants; re-export to prevent breakage

--- src/Node/FS/Constants.purs
+++ src/Node/FS/Constants.purs
@@ -34,3 +34,51 @@ foreign import appendCopyMode :: Fn2 CopyMode CopyMode CopyMode
 
 instance Semigroup CopyMode where
   append l r = runFn2 appendCopyMode l r
+
+data FileFlags
+  = R
+  | R_PLUS
+  | RS
+  | RS_PLUS
+  | W
+  | WX
+  | W_PLUS
+  | WX_PLUS
+  | A
+  | AX
+  | A_PLUS
+  | AX_PLUS
+
+instance showFileFlags :: Show FileFlags where
+  show R = "R"
+  show R_PLUS = "R_PLUS"
+  show RS = "RS"
+  show RS_PLUS = "RS_PLUS"
+  show W = "W"
+  show WX = "WX"
+  show W_PLUS = "W_PLUS"
+  show WX_PLUS = "WX_PLUS"
+  show A = "A"
+  show AX = "AX"
+  show A_PLUS = "A_PLUS"
+  show AX_PLUS = "AX_PLUS"
+
+instance eqFileFlags :: Eq FileFlags where
+  eq x y = show x == show y
+
+-- | Convert a `FileFlags` to a `String` in the format expected by the Node.js
+-- | filesystem API.
+fileFlagsToNode :: FileFlags -> String
+fileFlagsToNode ff = case ff of
+  R -> "r"
+  R_PLUS -> "r+"
+  RS -> "rs"
+  RS_PLUS -> "rs+"
+  W -> "w"
+  WX -> "wx"
+  W_PLUS -> "w+"
+  WX_PLUS -> "wx+"
+  A -> "a"
+  AX -> "ax"
+  A_PLUS -> "a+"
+  AX_PLUS -> "ax+"
